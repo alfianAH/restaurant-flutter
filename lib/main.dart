b9e52950af61abb8c0c963d9ded9b6314cda489f
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-17 22:42:15 +0800

    Add Home Page and styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/ui/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Restaurant',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: ,
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => HomePage(),
+      },
     );
   }
 }
