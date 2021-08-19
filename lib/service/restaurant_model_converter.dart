b3cc6c3b05dc83f500f8c3471f9fe7597d434f0e
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-19 21:32:21 +0800

    Add provider service and model converter

---
+++ lib/service/restaurant_model_converter.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:chopper/chopper.dart';
+import 'package:restaurant_app/data/model/restaurant_model.dart';
+
+class RestaurantModelConverter implements Converter{
+  @override
+  FutureOr<Request> convertRequest(Request request) {
+    final req = applyHeader(
+      request,
+      contentTypeKey,
+      jsonHeaders,
+      override: false
+    );
+
+    return encodeJson(req);
+  }
+
+  @override
+  FutureOr<Response<BodyType>> convertResponse<BodyType, InnerType>(Response<dynamic> response) {
+    return decodeJson<BodyType, InnerType>(response);
+  }
+
+  Request encodeJson(Request request){
+    var contentType = request.headers[contentTypeKey];
+
+    if(contentType != null && contentType.contains(jsonHeaders)){
+      return request.copyWith(body: json.encode(request.body));
+    }
+
+    return request;
+  }
+
+  Response<BodyType> decodeJson<BodyType, InnerType>(Response response){
+    var contentType = response.headers[contentTypeKey];
+    var body = response.body;
+
+    if(contentType != null && contentType.contains(jsonHeaders)){
+      body = utf8.decode(response.bodyBytes);
+    }
+
+    try{
+      var mapData = json.decode(body);
+      var restaurantModel = RestaurantModel.fromJson(mapData);
+
+      return response.copyWith(body: restaurantModel as BodyType);
+    } catch(e){
+      chopperLogger.warning(e);
+      return response.copyWith(body: body);
+    }
+  }
+}
